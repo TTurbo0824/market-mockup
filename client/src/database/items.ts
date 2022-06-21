2d6764e60e194c44a8aca73c034b66506c8eef81
TTurbo0824 <429why@gmail.com>
2022-06-21 11:03:45 +0900

    move: Relocate image files

--- client/src/database/items.ts
+++ client/src/database/items.ts
@@ -2,71 +2,120 @@ export const items = [
   {
     id: 0,
     itemName: '크리넥스 3겹 데코 소프트 30m x 30롤',
-    price: '20900',
+    price: 20900,
     category: '식품·생필품',
-    img: '../images/크리넥스.jpg'
+    img: '../images/items/크리넥스.jpg'
   },
   {
     id: 1,
     itemName: '기프트코드 1만원 / 구글 기프트카드',
-    price: '97500',
+    price: 97500,
     category: '여행·도서·e쿠폰',
-    img: '../images/구글.jpg'
+    img: '../images/items/구글.jpg'
   },
   {
     id: 2,
     itemName: 'CGV영화관람권',
-    price: '10000',
+    price: 10000,
     category: '여행·도서·e쿠폰',
-    img: '../images/영화.jpg'
+    img: '../images/items/영화.jpg'
   },
   {
     id: 3,
     itemName: '올리브 짜파게티 140g X 20입(박스)',
-    price: '15900',
+    price: 15900,
     category: '식품·생필품',
-    img: '../images/짜파게티.jpg'
+    img: '../images/items/짜파게티.jpg'
   },
   {
     id: 4,
     itemName: '물티슈 70매 X 10입(박스)',
-    price: '14000',
+    price: 14000,
     category: '식품·생필품',
-    img: '../images/물티슈.jpg'
+    img: '../images/items/물티슈.jpg'
   },
   {
     id: 5,
     itemName: '비타민C 1000 600정',
-    price: '37000',
+    price: 37000,
     category: '식품·생필품',
-    img: '../images/비타민c.jpg'
+    img: '../images/items/비타민c.jpg'
   },
   {
     id: 6,
     itemName: '무선 게이밍 마우스',
-    price: '120000',
+    price: 120000,
     category: '컴퓨터·디지털·가전',
-    img: '../images/마우스.jpg'
+    img: '../images/items/마우스.jpg'
   },
   {
     id: 7,
     itemName: '향수 100ml',
-    price: '150000',
+    price: 150000,
     category: '패션의류·잡화·뷰티',
-    img: '../images/향수.jpg'
+    img: '../images/items/향수.jpg'
   },
   {
     id: 8,
     itemName: '드라이버',
-    price: '20000',
+    price: 20000,
     category: '자동차·공구',
-    img: '../images/드라이버.jpg'
+    img: '../images/items/드라이버.jpg'
   },
   {
     id: 9,
     itemName: '고양이 사료 5kg',
-    price: '20000',
+    price: 20000,
     category: '홈데코·문구·취미·반려',
-    img: '../images/사료.jpg'
+    img: '../images/items/사료.jpg'
+  },
+  {
+    id: 10,
+    itemName: '자동차 클리너 세트',
+    price: 18000,
+    category: '자동차·공구',
+    img: '../images/items/클리너.jpg'
+  },
+  {
+    id: 11,
+    itemName: '논 슬립 아령 3kg',
+    price: 15000,
+    category: '스포츠·건강·렌탈',
+    img: '../images/items/아령.jpg'
+  },
+  {
+    id: 12,
+    itemName: '토스터기',
+    price: 40000,
+    category: '컴퓨터·디지털·가전',
+    img: '../images/items/토스터기.jpg'
+  },
+  {
+    id: 13,
+    itemName: '수분크림',
+    price: 44000,
+    category: '패션의류·잡화·뷰티',
+    img: '../images/items/수분크림.jpg'
+  },
+  {
+    id: 14,
+    itemName: '탄산수 350ml 24PET',
+    price: 16000,
+    category: '식품·생필품',
+    img: '../images/items/탄산수.jpg'
+  },
+  {
+    id: 15,
+    itemName: '등산스틱',
+    price: 40000,
+    category: '스포츠·건강·렌탈',
+    img: '../images/items/등산스틱.jpg'
+  },
+  {
+    id: 16,
+    itemName: '사무용 의자',
+    price: 370000,
+    category: '홈데코·문구·취미·반려',
+    img: '../images/items/의자.jpg'
   }
 ];
