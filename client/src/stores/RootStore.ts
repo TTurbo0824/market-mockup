f7a261150dbf22d59796f81941f897cc2fac80e0
TTurbo0824 <429why@gmail.com>
2022-06-27 14:19:00 +0900

    modify: Stores

--- client/src/stores/RootStore.ts
+++ client/src/stores/RootStore.ts
@@ -1,6 +1,7 @@
 import ItemStore from './ItemStore';
 import CartStore from './CartStore';
 import UserStore from './UserStore';
+import AdminStore from './AdminStore';
 import ModalStore from './ModalStore';
 
 export default class RootStore {
@@ -8,11 +9,13 @@ export default class RootStore {
     this.itemStore = new ItemStore(this);
     this.cartStore = new CartStore(this);
     this.userStore = new UserStore(this);
+    this.adminStore = new AdminStore(this);
     this.modalStore = new ModalStore(this);
   }
 
   itemStore: ItemStore;
   cartStore: CartStore;
   userStore: UserStore;
+  adminStore: AdminStore;
   modalStore: ModalStore;
 }
--- client/src/stores/UserStore.ts
+++ client/src/stores/UserStore.ts
@@ -1,42 +1,72 @@
-import { action, makeAutoObservable } from 'mobx';
+import { makeAutoObservable, computed, toJS, action } from 'mobx';
 import { makePersistable } from 'mobx-persist-store';
 import RootStore from './RootStore';
 
 export interface User {
-  id: number;
-  userName: string;
-  gender: string;
-  age: string;
-  occupation: string;
+  isAdmin: boolean;
+  token: string | null;
+  id: number | null;
+  userName: string | null;
+  password: string | null;
+  userStatus: string | null;
+  signupDate: string | null;
+  dormantDate: string | null;
 }
 
 export default class UserStore {
   constructor(RootStore: RootStore) {
     makeAutoObservable(this, {
       signIn: action,
-      signOut: action
+      signOut: action,
+      getUserType: computed,
+      getUserInfo: computed
     });
 
     makePersistable(this, {
       name: 'UserStore',
-      properties: ['user'],
+      properties: ['userType', 'userInfo'],
       storage: window.localStorage
     });
   }
 
-  user = {
-    id: 0,
-    userName: 'testuser',
-    gender: 'male',
-    age: '20-39',
-    occupation: 'student'
+  userType = 'nonuser';
+
+  userInfo: User = {
+    isAdmin: false,
+    token: null,
+    id: null,
+    userName: null,
+    password: null,
+    userStatus: null,
+    signupDate: null,
+    dormantDate: null
   };
 
-  signIn(user: User[]) {
-    console.log(user);
+  signIn(user: User) {
+    this.userType = user.isAdmin === true ? 'admin' : 'user';
+    this.userInfo = user;
   }
 
-  signOut(user: User[]) {
-    console.log(user);
+  signOut() {
+    this.userType = 'nonuser';
+
+    this.userInfo = {
+      isAdmin: false,
+      token: null,
+      id: null,
+      userName: null,
+      password: null,
+      userStatus: null,
+      signupDate: null,
+      dormantDate: null
+    };
+  }
+
+  get getUserType() {
+    return toJS(this.userType);
+  }
+
+  get getUserInfo() {
+    return toJS(this.userInfo);
   }
 }
